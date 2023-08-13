0d19b83833f1bb5ce5e3af4d4dbe086a1520b2f9
Faizalbabu <139562297+Faizalbabu@users.noreply.github.com>
2023-08-13 22:55:10 +0530

    Delete src directory

--- src/pages/ProfilePage.js
+++
@@ -1,7 +0,0 @@
-function ProfilePage(){
-    return(
-        <h1 style={{textAlign:"center",position:"relative",top:"100px",color:"black",fontWeight:600}}>Welcome</h1>
-    )
-}
-
-export default ProfilePage;
